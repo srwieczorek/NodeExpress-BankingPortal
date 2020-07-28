4cb3571d99f0fa8cce017c678c226b1544c590cc
Steve Wieczorek <xtremefire569@gmail.com>
2020-07-28 13:15:09 -0400

    module 3

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
 const accountData = fs.readFileSync('src/json/accounts.json', {encoding: 'utf8'});
 const userData = fs.readFileSync('src/json/users.json', {encoding: 'utf8'});
@@ -22,5 +23,30 @@ app.get('/savings', (req, res) => res.render('account', {account: accounts.savin
 app.get('/checking', (req, res) => res.render('account', {account: accounts.checking}));
 app.get('/credit', (req, res) => res.render('account', {account: accounts.credit}));
 app.get('/profile', (req, res) => res.render('profile', {user: users[0]}));
+app.get('/transfer', (req, res) => res.render('transfer'));
+app.post('/transfer', (req, res) => {
+    const { from, to, amount } = req.body;
+    const acntFrom = accounts[from];
+    const acntTo = accounts[to];
+
+    acntFrom.balance = acntFrom.balance - amount;
+    acntTo.balance = acntTo.balance + parseInt(amount);
+
+    const accountsJSON = JSON.stringify(accounts);
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, {encoding: 'utf8'});
+    res.render('transfer', {message: 'Transfer Completed'});
+});
+app.get('/payment', (req, res) => res.render('payment', {account: accounts.credit}));
+app.post('/payment', (req, res) => {
+    const { amount } = req.body;
+    accounts.credit.balance = accounts.credit.balance - amount;
+    accounts.credit.available = parseInt(accounts.credit.available) + parseInt(amount);
+
+    const accountsJSON = JSON.stringify(accounts);
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, {encoding: 'utf8'});
+    res.render('payment', {message: 'Payment Successful', account: accounts.credit});
+})
+
+
 
 app.listen(3000, () => console.log('PS Project Running on port 3000!'));
