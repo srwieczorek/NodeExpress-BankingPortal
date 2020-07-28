5680a33531c0820b79e00a138333ffd918a0b771
Steve Wieczorek <xtremefire569@gmail.com>
2020-07-27 21:13:16 -0400

    set views dir

--- src/app.js
+++ src/app.js
@@ -4,3 +4,6 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
